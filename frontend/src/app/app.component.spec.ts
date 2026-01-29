fead4ce523071a8a4248320263857ab9e2b581c7
Winston <wbowen711@gmail.com>
2026-01-29 17:19:12 -0500

    chore(frontend): add test bootstrap and node engine

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
