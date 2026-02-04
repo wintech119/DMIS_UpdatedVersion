0b43c5747fe015bbee30395b537c81041cd676a0
Winston <wbowen711@gmail.com>
2026-02-04 13:24:14 -0500

    Update replenishment wizard flow

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 
 import { StockStatusDashboardComponent } from './replenishment/stock-status-dashboard/stock-status-dashboard.component';
 import { NeedsListPreviewComponent } from './replenishment/needs-list-preview/needs-list-preview.component';
+import { NeedsListWizardComponent } from './replenishment/needs-list-wizard/needs-list-wizard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'replenishment/dashboard', pathMatch: 'full' },
   { path: 'replenishment/dashboard', component: StockStatusDashboardComponent },
+  { path: 'replenishment/needs-list-wizard', component: NeedsListWizardComponent },
   { path: 'replenishment/needs-list-preview', component: NeedsListPreviewComponent },
   { path: '**', redirectTo: 'replenishment/dashboard' }
 ];
