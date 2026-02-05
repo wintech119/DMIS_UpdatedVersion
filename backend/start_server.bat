c36b00e036fc048514f9aafb07cd9975b6cbca14
Winston <wbowen711@gmail.com>
2026-02-05 09:17:49 -0500

    Quote env vars in start_server.bat

--- backend/start_server.bat
+++ backend/start_server.bat
@@ -1,9 +1,9 @@
 @echo off
-set DB_NAME=dmis
-set DB_USER=postgres
-set DB_PASSWORD=Excellence!00
-set DB_HOST=localhost
-set DB_PORT=5432
-set DJANGO_DEBUG=1
+SET DB_NAME="dmis"
+SET DB_USER="postgres"
+SET DB_PASSWORD="Excellence!00"
+SET DB_HOST="localhost"
+SET DB_PORT="5432"
+SET DJANGO_DEBUG="1"
 
 python manage.py runserver
